dd8288cfd24a119f32b3451cbff0c5177bf615b1
trentungard <trentungard@gmail.com>
2020-08-26 21:57:45 -0400

    made code more modular, still need to fix default errors

--- app.js
+++ app.js
@@ -1,46 +1,5 @@
-const https = require('https');
-const http = require('http');
-
-function printMessage(userName, badgeCount, points){
-  const message =`${userName} has ${badgeCount} total badges and ${points} total points`;
-  console.log(message)
-};
-
-function printError(error){
-  console.error(error.message);
-}
-
-function getProfile(username){
-  try{
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, (res) => {
-    if(res.statusCode === 200){
-      let body = "";
-    res.on('data', data => {
-      body += data.toString();
-    });
-      
-    res.on('end', () => {
-           try{
-           const profile = JSON.parse(body);
-            printMessage(username, profile.badges.length, profile.points.total);
-           } catch(error) {
-            printError(error)       
-           }
-    });
-    } else {
-      const message = `There was an error getting the profile for ${username} response (${http.STATUS_CODES[res.statusCode]})`;
-      const statusCodeError = new Error(message)
-      printError(statusCodeError);
-    }
-   });
-  
-  request.on('error', printError(error));
-  
-  } catch (error) { 
-     printError(error)
-  };
-};
+const profile = require('./profile'); 
 
 const users = ['trentungard'];
 users.push(process.argv.slice(2));
-users.forEach(getProfile);
+users.forEach(profile.get);
